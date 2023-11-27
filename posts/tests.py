edd5729d03b8492aa6adea2c457fa3c61d52dbfc
= <=>
2023-11-27 22:10:31 +0000

    added tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 # Create your tests here.
@@ -6,7 +7,15 @@ from .models import Post
 class PostTests(TestCase):
     @classmethod
     def setUpData(cls):
-        cls.post = Post.objects.create(text="This is a test")
+        cls.post = Post.objects.create(text="This is a test!")
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test")
+    def test_url_exists_at_correct_location(self): # new
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    def test_homepage(self): # new
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
